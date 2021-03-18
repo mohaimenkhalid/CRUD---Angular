9549d36cb6fc8c65cfd45a93df9f5e58ba07d75f
mohaimenkhallid <mohaimen707@gmail.com>
2021-03-18 11:10:23 +0600

    Setup project with necessary file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetsilsFormComponent } from './payment-details/payment-detsils-form/payment-detsils-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetsilsFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/payment-details/payment-detsils-form/payment-detsils-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-detsils-form',
+  templateUrl: './payment-detsils-form.component.html',
+  styles: [
+  ]
+})
+export class PaymentDetsilsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
